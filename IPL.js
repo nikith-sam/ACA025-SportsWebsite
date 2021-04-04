0f643828cb769a9e5f14473958583537c05b8a17
nikith-sam <80050329+nikith-sam@users.noreply.github.com>
2021-04-04 11:20:37 +0530

    Add files via upload

---
+++ IPL.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAiqC4YZUm-0Ou7Nn7mXXuRjyif3N4k0DY",
+    authDomain: "vk-ipl.firebaseapp.com",
+    databaseURL: "https://vk-ipl-default-rtdb.firebaseio.com/",
+    projectId: "vk-ipl",
+    storageBucket: "vk-ipl.appspot.com",
+    messagingSenderId: "99410861805",
+    appId: "1:99410861805:web:351f86da8116feac28f6e4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
